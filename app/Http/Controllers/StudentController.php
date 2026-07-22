ca03d56edb107e51a8f243060a0d79f8e90f09c2
TUF GAMING A15 <devonaustin042@gmail.com>
2026-07-22 13:33:42 +0700

    Students Part

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,11 +8,36 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return "Ini adalah halaman daftar siswa";
+        return "Menampilkan halaman daftar siswa";
     }
 
     public function show($id)
     {
-        return "Menampilkan detail siswa dengan ID: " . $id;
+        return "Menampilkan siswa dengan ID: {$id}";
+    }
+
+    public function create()
+    {
+        return "Menampilkan halaman tambah siswa";
+    }
+
+    public function edit(string $id)
+    {
+        return "Menampilkan halaman edit siswa dengan ID: {$id}";
+    }
+
+  public function store()
+    {
+        return "Melakukan penambahan data siswa";
+    }
+
+    public function update(string $id)
+    {
+        return "Melakukan update data siswa dengan ID: {$id}";
+    }
+
+    public function destroy(string $id)
+    {
+        return "Menghapus data siswa dengan ID: {$id}";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,25 @@ Route::get('/', function () {
 
 // Managemen Siswa
 Route::name('students.')->prefix('students')->group(function(){
+
+    // Halaman daftar siswa
     Route::get('/', [StudentController::class,'index'])->name('index');
 
+    // Halaman tambah siswa
+    Route::get('/create', [StudentController::class,'create'])->name('create');
+
+     // Halaman edit siswa
+    Route::get('/{id}/edit', [StudentController::class,'edit'])->name('edit');
+
+    // Halaman detail siswa
     Route::get('/{id}', [StudentController::class,'show'])->name('show');
+
+    // Logika Tambah Siswa
+    Route::post('/', [StudentController::class,'store'])->name('store');
+
+    // Logika Edit Siswa
+    Route::put('/{id}', [StudentController::class,'update'])->name('update');
+
+    // Logika Hapus Siswa
+    Route::delete('/{id}', [StudentController::class,'destroy'])->name('destroy');
 });
