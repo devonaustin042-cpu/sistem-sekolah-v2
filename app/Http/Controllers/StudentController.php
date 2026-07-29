50a8877c792069850d955cb6861ebb9305cbddfa
TUF GAMING A15 <devonaustin042@gmail.com>
2026-07-29 13:51:27 +0700

    implement view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,22 +8,55 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return "Menampilkan halaman daftar siswa";
+        $title = "Sistem Sekolah - Daftar Siswa";
+        $students = [
+            [
+                'id' => 1,
+                'nis' => '2210001',
+                'name' => 'Andi',
+                'class' => 'XII TKJ 3',
+                'major' => 'TKJ'
+            ], 
+            [
+                'id' => 2,
+                'nis' => '2210002',
+                'name' => 'Budi',
+                'class' => 'XII AKL 1',
+                'major' => 'AKL'
+            ],
+        ];
+
+        return view('students.index', [
+            'title' => $title,
+            'students' => $students
+        ]);
     }
 
     public function show($id)
     {
-        return "Menampilkan siswa dengan ID: {$id}";
+        $title = "Sistem Sekolah - Detail Siswa";
+
+        return view('students.show', [
+            'title' => $title
+        ]);
     }
 
     public function create()
     {
-        return "Menampilkan halaman tambah siswa";
+        $title = "Sistem Sekolah - Tambah Siswa";
+
+        return view('students.create', [
+            'title' => $title
+        ]);
     }
 
     public function edit(string $id)
     {
-        return "Menampilkan halaman edit siswa dengan ID: {$id}";
+        $title = "Sistem Sekolah - Edit Siswa";
+
+        return view('students.edit', [
+            'title' => $title
+        ]);
     }
 
   public function store()
