dfd469cb807a4c5481ffe6c540cc4a45537118ae
TUF GAMING A15 <devonaustin042@gmail.com>
2026-08-04 15:59:13 +0700

    commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,9 +3,6 @@
 @section('title', $title)
 
 @section('content')
-
-
-
     <div class="mb-8 flex items-end justify-between border-b border-[#E5E3DB] pb-5">
     <div>
         <p class="mb-1 text-[11px] uppercase tracking-[0.2em] text-[#A16207]">Tahun Ajaran 2025/2026</p>
